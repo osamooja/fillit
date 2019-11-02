b719f9d348608807470fb57544c28109e9ee2dee
lsjoberg <lucasjoberg@gmail.com>
2019-11-02 15:04:06 +0200

    solverAndMap

---
+++ map.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsjoberg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/02 11:37:48 by lsjoberg          #+#    #+#             */
+/*   Updated: 2019/11/02 15:03:36 by lsjoberg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+/*
+** Get the square root and round it up for starting map map_size.
+** Example: If the number of blocks in pieces would require a 2.3 x 2.3 grid
+** then it needs to round up to be 3x3 minimum to fit.
+** In that case this function would return 3.
+*/
+
+int		round_up_sq_rt(int num)
+{
+	int	map_size;
+
+	map_size = 2;
+	while (map_size * map_size < num)
+		map_size++;
+	return (map_size);
+}
+
+/*
+** Use malloc to make space for the map.
+** Use malloc to make space for 2D map array.
+** then you need to malloc out the rows of the map based on map_size as well,
+** plus newline with strnew(). Set it all to .
+*/
+
+t_map	*new_map(int map_size)
+{
+	t_map	*map;
+	int		i;
+
+	map = (t_map *)ft_memalloc(sizeof(t_map));
+	map->array = (char**)ft_memalloc(sizeof(char*) * map_size);
+	i = 0;
+	while (i < map_size)
+	{
+		map->array[i] = ft_strnew(map_size);
+		ft_memset(map->array[i], '.', map_size);
+		i++;
+	}
+	return (map);
+}
+
+size_t	count_pieces(t_piece *piecelist)
+{
+	size_t	count;
+
+	count = 0;
+	while (piecelist)
+	{
+		piecelist = piecelist->next;
+		count++;
+	}
+	return (count);
+}
+
+/*
+** Print the map one row at a time followed by newline.
+*/
+
+void	print_map(t_map *map, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		ft_putstr(map->array[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+/*
+** frees the map.
+*/
+
+void	free_map(t_map *map, int map_size)
+{
+	int	i;
+
+	i = 0;
+	while (i < map_size)
+	{
+		ft_memdel((void **)&(map->array[i]));
+		i++;
+	}
+	ft_memdel((void **)&(map->array));
+	ft_memdel((void **)&map);
+}
---
+++ solver.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solver.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsjoberg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/02 14:21:12 by lsjoberg          #+#    #+#             */
+/*   Updated: 2019/11/02 15:02:53 by lsjoberg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int		overlap(t_map *map, t_piece *piece)
+{
+	int i;
+	int x;
+	int y;
+
+	i = 0;
+	x = 0;
+	y = 0;
+	x = piece->blockcoords[i] + piece->x_offset;
+	y = piece->blockcoords[i + 1] + piece->y_offset;
+	while (i <= 6 && map->array[y][x] == '.')
+	{
+		i += 2;
+		x = piece->blockcoords[i] + piece->x_offset;
+		y = piece->blockcoords[i + 1] + piece->y_offset;
+	}
+	return (i != 8);
+}
+
+void	place(t_piece *piece, t_map *map, char letter)
+{
+	int	i;
+	int x;
+	int y;
+
+	i = 0;
+	x = 0;
+	y = 0;
+	while (i <= 6)
+	{
+		x = piece->blockcoords[i] + piece->x_offset;
+		y = piece->blockcoords[i + 1] + piece->y_offset;
+		map->array[y][x] = letter;
+		i += 2;
+	}
+}
+
+int		in_bounds(t_piece *piece, int map_size, char axis)
+{
+	if (axis == 'y')
+		return (piece->blockcoords[1] + piece->y_offset < map_size &&
+				piece->blockcoords[3] + piece->y_offset < map_size &&
+				piece->blockcoords[5] + piece->y_offset < map_size &&
+				piece->blockcoords[7] + piece->y_offset < map_size);
+	else
+		return (piece->blockcoords[0] + piece->x_offset < map_size &&
+				piece->blockcoords[2] + piece->x_offset < map_size &&
+				piece->blockcoords[4] + piece->x_offset < map_size &&
+				piece->blockcoords[6] + piece->x_offset < map_size);
+}
+
+/*
+** While in bounds, if it doesn't overlap place the piece. Use recursion
+** to check if the rest can fit with the current piece placed where it is.
+**  Otherwise it backtracks and moves current piece
+** then checks if all the pieces that come after fit with current piece moved.
+** place() can either place the piece or place '.' to clear a piece.
+*/
+
+int		solve_map(t_map *map, t_piece *piece, int map_size)
+{
+	if (!piece)
+		return (1);
+	piece->x_offset = 0;
+	piece->y_offset = 0;
+	while (in_bounds(piece, map_size, 'y'))
+	{
+		while (in_bounds(piece, map_size, 'x'))
+		{
+			if (!overlap(map, piece))
+			{
+				place(piece, map, piece->pieceletter);
+				if (solve_map(map, piece->next, map_size))
+					return (1);
+				else
+				{
+					place(piece, map, '.');
+				}
+			}
+			piece->x_offset++;
+		}
+		piece->x_offset = 0;
+		piece->y_offset++;
+	}
+	return (0);
+}
+
+/*
+** Start with the smallest map map_size for number of blocks in pieces.
+*/
+
+void	solve(t_piece *piecelist)
+{
+	t_map	*map;
+	int		map_size;
+
+	map_size = round_up_sq_rt(count_pieces(piecelist) * 4);
+	map = new_map(map_size);
+	while (!solve_map(map, piecelist, map_size))
+	{
+		free_map(map, map_size);
+		map_size++;
+		map = new_map(map_size);
+	}
+	print_map(map, map_size);
+	free_map(map, map_size);
+}
